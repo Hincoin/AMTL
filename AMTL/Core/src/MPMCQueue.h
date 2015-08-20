c2d394e072bef34d0ea02c34c61540f981d2a10b
Alejandro <lucenadeveloper@gmail.com>
2015-08-20 10:51:34 -0400

    added lock free mpmc queue

--- AMTL/Core/src/MPMCQueue.h
+++ AMTL/Core/src/MPMCQueue.h
@@ -1,5 +1,5 @@
-#include <memory>
 #include <atomic>
+#include <memory>
 
 namespace amtl
 {
@@ -25,8 +25,8 @@ namespace amtl
 				an external count. When that thread leaves (will not touch the node anymore), it decrements an internal counter. 
 				The sum of the internal	and external counts should therefore determine if reclaiming memory is safe. This safeness
 				is realized after 2 events -- after all contending threads decrement the internal ref count and after the "sucessfull"
-				thread increments the internal ref count such that it can reach 0. This notion of a "sucessful" thream comes from the fact that
-				although there may be many threads that attempt to push concurrently, and many threads that try to pop concurrently, only 1 
+				thread increments the internal ref count such that it can reach 0. This notion of a "sucessful" thread comes from the fact that
+				although there may be many threads that attempt to push concurrently or many threads that try to pop concurrently, only 1 
 				single thread out of the push()ers and 	1 single thread out of the pop()ers will be able to perform the action at a time due to 
 				the internal synchronization of atomics.
 
@@ -35,13 +35,24 @@ namespace amtl
 			    contending with another pop()-ing thread to update	head. Since only 1 of these contending threads can actually modify head at a time, 
 			    that "winning" thread is deemed "sucessful" and has the responsiblity of _incrementing_ the internal count to try and bring it 0. 
 			    Specifically, the internal ref count is incremented by the value (external count - 2).  This number comes from the fact that
-			    there can be at _most_ external_count-2 other threads touching this node.  "Why", you ask? The "successfull" threads owns a reference,
-			    so that leaves external_count-1 other threads, BUT, every node begins life with an external_count of 1.
-		
+			    there can be at _most_ external_count-2 other threads touching this node.  Why - 2? The "successfull" threads owns a reference,
+			    so that leaves external_count-1 other threads, BUT, every node begins life with an external_count of 1. This seemingly unecessary
+			    reference count is needed to prevent race conditions between pushers and poppers that contend on the same node. Because the push()ers 
+			    _collectively_ own a single reference count, and the poppers _collectively_ also own a single reference count, this prevents a node
+			    from being deleted out from a push()-ing thread that simply was scheduled poorly and left to the fate of pop()-ers bringing the internal
+			    ref count down to 0. However, since that lonely, poorly-scheduled thread is _still_ a push()-ing thread, it continues to own an external count
+			    -- thus preventing the node from being deleted out from under its feet.
+
+			    Alas, this alone doesn't keep the queue safe. Since the queue can be modified by _both_ a head and tail, another value is needed to signal the
+			    relinquishment of a head/tail's ownership from a node. Since it only needs to be a bit, this value can be implemented as a 2 bit value. And, this
+			    queue being lock-free, the (30 or 62) bits can be used to implement the internal count. This structure is represented below by struct node_counter.
+			    It's total size is that of an int, so lock-free operations on it have the same guarantee as std::atomic<int>::is_lock_free()
+			 
 			*/
+
 			struct node_counter
 			{
-				unsigned internal_count : 30;
+				unsigned internal_count : sizeof(int)-2;
 				unsigned external_counters : 2;
 			};
 
@@ -49,9 +60,9 @@ namespace amtl
 
 			struct node
 			{
-				std::atomic<T*> data;
-				std::atomic<node_counter> node_count;
-				std::atomic<counted_node_pointer> next;
+				std::atomic<T*> data; 					// pushing onto the queue is done by atomically CAS-ing this data field, hence the need for atomic<T*>
+				std::atomic<node_counter> node_count;	// reference count operations certainly need be atomic as they are read/written by multiple threads
+				counted_node_pointer next;				// next is only ever modified by a single thread, so atomicity is not needed.
 
 				node()
 				{
@@ -59,7 +70,7 @@ namespace amtl
 
 					node_counter initial_count;
 					initial_count.internal_count = 0;
-					initial_count.external_counters = 2;// all nodes start off with 2 owning refs.
+					initial_count.external_counters = 2;
 					node_count.store(initial_count);
 
 					data = nullptr;
@@ -91,7 +102,7 @@ namespace amtl
 			std::atomic<counted_node_pointer> head;
 			std::atomic<counted_node_pointer> tail;
 
-			void increase_ref_count(counted_node_pointer& old_node, std::atomic<counted_node_pointer>& marker)
+			void increase_ref_count(counted_node_pointer& old_node, std::atomic<counted_node_pointer>& marker) noexcept
 			{
 				// spin in a CAS loop until the marker external count can be properly incremented by this thread
 
@@ -106,7 +117,8 @@ namespace amtl
 
 			}
 
-			void free_external_count(counted_node_pointer& winner_thread_node)
+
+			void free_external_count(counted_node_pointer& winner_thread_node) noexcept
 			{
 				const int num_increase = winner_thread_node.external_count - 2;
 
@@ -128,92 +140,41 @@ namespace amtl
 				}
 			}
 
-			void set_tail_node(counted_node_pointer& old_tail, counted_node_pointer& new_tail)
+			// push_impl is the internal worker function that actually pushes data onto the queue.
+			// it does so by atomically CAS-ing tail's node_ptr->data field. If a thread can swap
+			// node_ptr->data from nullptr to valid data, that thread is now responsible for moving tail
+
+			// other threads must keep spinning until tail is updated.
+			// TODO: Implement helping-threads that create dummy nodes, push tail.
+
+
+			void push_impl(std::unique_ptr<node> node_ptr, std::unique_ptr<T> data_ptr) noexcept
 			{
-				// tail _should_ be old_tail. If an atomic CAS finds that it is, swap it to new_tail.
-				// This needs to be done in a loop until the swap can be guaranteed. However,
-				// a naive loop will not be enough.  It's possible that another thread also updates
-				// tail while we're trying to modify it. This leads us to the case where a CAS
-				// iteration failes, sets old_tail to what it found (which may be a brand new node),
-				// then resets tail to new_tail, effectively wiping out the new node, or even worse
-				// it may create a queue state where push()-ing threads spin indefinitely.
-
-				// to prevent this, we need to save that node pointer that old_tail has upon entry, and check it
-				// upon each failure of the CAS loop. There are 2 reasons why a CAS attempt fails:
-				//	- Spurious failure
-				//  - Legitamite failure
-
-				// Upon either one, its important to see if the value of old_tail.node_ptr is the same as when 
-				// we called this function. Otherwise, the invalid queue state may form.
-
-
-
-				node* const saved_ptr = old_tail.node_ptr;
-				while(!tail.compare_exchange_weak(old_tail,new_tail) && old_tail.node_ptr == saved_ptr);
-
-				// the while loop will exit either because of a CAS success or a CAS failure w/ changed pointers
-				// in the former case, the initial attempt at CAS succeeded (with the exception of spurious failures)
-
-				// in the latter case, a CAS failed and old_tail got updated to a node without the same pointer. 
-				// in that case, the loop needs to break.
-
-				if(saved_ptr == old_tail.node_ptr)
-				{
-					// this thread got to set tail
-					free_external_count(old_tail);
-				}
-				else
-				{
-					saved_ptr->release_reference();
-				}
-
-
-			}
-
-
-			void push_impl(std::unique_ptr<T> data_ptr)
-			{
-				std::unique_ptr<node> node_ptr(new node);
 
 				counted_node_pointer  new_tail;
 				new_tail.external_count = 1;
+				new_tail.node_ptr = node_ptr.get();
 
 				counted_node_pointer old_tail = tail.load();
 				for(;;)
 				{
-					new_tail.node_ptr = node_ptr.get();
+					
 					increase_ref_count(old_tail,tail);
 					T* old_data = nullptr;
 
 					if(old_tail.node_ptr->data.compare_exchange_strong(old_data,data_ptr.get()))
 					{
-						
-						counted_node_pointer old_next = {0};
-						if(! old_tail.node_ptr->next.compare_exchange_strong(old_next,new_tail))
-						{
-							// some other thread already set the dummy node
-							std::unique_ptr<node>{}.swap(node_ptr);
-							new_tail = old_next;
-						}
-
-						set_tail_node(old_tail,new_tail);
-						data_ptr.release();
+						old_tail.node_ptr->next = new_tail;
+						old_tail = tail.exchange(new_tail);
+						free_external_count(old_tail);
+						data_ptr.release();	
 						node_ptr.release();
-						return;
+						break;
+					
+					}
 
-					}
-					else
-					{
-						// help out the "winner" thread by setting the dummy next node
-						counted_node_pointer old_next = {0};
-						if(old_tail.node_ptr->next.compare_exchange_strong(old_next,new_tail))
-						{
-							old_next = new_tail;
-							node_ptr.release();
-							node_ptr.reset(new node);
-						}
-						set_tail_node(old_tail,old_next);
-					}
+					old_tail.node_ptr->release_reference();
+					
 				}
 			}
 
@@ -232,59 +193,53 @@ namespace amtl
 			MPMCQueue& operator= (const MPMCQueue& other) = delete;
 			MPMCQueue(const MPMCQueue& other) = delete;
 
-			// moving takes the donor's head/tail and nulls them out of the donor
-			MPMCQueue(MPMCQueue&& other) : head(other.head.load()) , tail(other.tail.load())
-			{
-				other.head = {0,0};
-				other.tail = {0,0};
-			}
+			// disable move
 			MPMCQueue& operator=(MPMCQueue&& other) = delete;
 
 
-			~MPMCQueue()
+			~MPMCQueue() 
 			{
-				// node* head_ptr = head.load().node_ptr;
-				// node* tail_ptr = tail.load().node_ptr;
+				node* head_ptr = head.load().node_ptr;
+				node* tail_ptr = tail.load().node_ptr;
 
-				// // if there's any remaining data items in the queue upon destruction,
-				// // iterate through the linked list and manage each T* by wrapping it in a unique_ptr.
-				// // The loop should only extend up until tail's ptr because the class invariants
-				// // maintain that after a succesful call to push(), tail points to a dummy node
-				// // without any valid data.
-				// while(head_ptr != tail_ptr)
-				// {
-				// 	node* ptr_next = head_ptr->next.load().node_ptr;
-				// 	std::unique_ptr<T> head_data{head_ptr->data.load()};
-				// 	delete head_ptr;
-				// 	head_ptr = ptr_next;
+				// if there's any remaining data items in the queue upon destruction,
+				// iterate through the linked list and manage each T* by wrapping it in a unique_ptr.
+				// The loop should only extend up until tail's ptr because the class invariants
+				// maintain that after a succesful call to push(), tail points to a dummy node
+				// without any valid data.
+				while(head_ptr != tail_ptr)
+				{
+					node* ptr_next = head_ptr->next.node_ptr;
+					std::unique_ptr<T> head_data{head_ptr->data.load()};
+					delete head_ptr;
+					head_ptr = ptr_next;
 
-				// }
-				// // no more data to delete, just delete tail's node
-				// delete head_ptr;
+				}
+				// no more data to delete, just delete tail's node
+				delete head_ptr;
 			}
 
 
-			void push(const T& copy)
-			{
-				std::unique_ptr<T> data_ptr { new T{copy} };
-				push_impl(std::move(data_ptr));
-			}
+			
+			// push will attempt to append a new data item onto the queue in a thread-safe, lock-free manner.
+			// appending will construct a T by perfectly-forward along the arguments passed in, to T's constructors.
+			// The behavior is undefined unless all arguments are valid.
 
-			void push(T&& move)
-			{
-				std::unique_ptr<T> data_ptr { new T{std::move(move)} };
-				push_impl(std::move(data_ptr));
-			}
-
+			// push provides the strong exception safety guarantee
 			template<typename... CtorArgs>
 			void push(CtorArgs&&... ctor_args)
 			{
-				std::unique_ptr<T> data_ptr{ new T{std::forward<CtorArgs>(ctor_args)...} };
-				push_impl(std::move(data_ptr));
+				std::unique_ptr<node> node_ptr{ new node};
+				std::unique_ptr<T> 	  data_ptr{ new T{std::forward<CtorArgs>(ctor_args)...} };
+				push_impl(std::move(node_ptr),std::move(data_ptr));
 			}
 			
 
-			std::unique_ptr<T> pop()
+			// pop will attempt to remove the next item in the queue, and return a unique_ptr to the data.
+			// If the queue is full, a default-constructed unique_ptr<T> is returned, instead.
+			// pop will not emit an exception.
+
+			std::unique_ptr<T> pop() noexcept
 			{
 				counted_node_pointer old_head = head.load();
 				for(;;)
@@ -299,10 +254,9 @@ namespace amtl
 						return {};
 					}
 
-					counted_node_pointer next = ptr->next.load();
-					if(head.compare_exchange_strong(old_head,next))
+					if(head.compare_exchange_strong(old_head,ptr->next))
 					{
-						std::unique_ptr<T> data(ptr->data.exchange(nullptr));
+						std::unique_ptr<T> data(ptr->data.load());
 						free_external_count(old_head);
 						return data;
 					}
